e32f3a28ebad569d5bd7f7a76b4eec9b9d0d7205
Teagrin Forde <teagrinb@gmail.com>
2022-07-08 11:17:29 -0400

    add dates in js

--- resources/index.js
+++ resources/index.js
@@ -1,7 +1,15 @@
+//display dates
 var todayDateEl = moment().format('ll');
 var dayOneEl = moment(todayDateEl).add(1, 'days').format('ll');
 var dayTwoEl = moment(todayDateEl).add(2, 'days').format('ll');
 var dayThreeEl = moment(todayDateEl).add(3, 'days').format('ll');
 var dayFourEl = moment(todayDateEl).add(4, 'days').format('ll');
 var dayFiveEl = moment(todayDateEl).add(5, 'days').format('ll');
+
+document.getElementById('dateInfo').innerHTML = todayDateEl;
+document.getElementById('dateOne').innerHTML = dayOneEl;
+document.getElementById('dateTwo').innerHTML = dayTwoEl;
+document.getElementById('dateThree').innerHTML = dayThreeEl;
+document.getElementById('dateFour').innerHTML = dayFourEl;
+document.getElementById('dateFive').innerHTML = dayFiveEl;
 
