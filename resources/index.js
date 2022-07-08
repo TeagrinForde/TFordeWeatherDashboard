0ad921c460c79bb3a5f68854ea1a19b3f10a81aa
Teagrin Forde <teagrinb@gmail.com>
2022-07-08 09:34:58 -0400

    add moment variables in js

---
+++ resources/index.js
@@ -0,0 +1,7 @@
+var todayDateEl = moment().format('ll');
+var dayOneEl = moment(todayDateEl).add(1, 'days').format('ll');
+var dayTwoEl = moment(todayDateEl).add(2, 'days').format('ll');
+var dayThreeEl = moment(todayDateEl).add(3, 'days').format('ll');
+var dayFourEl = moment(todayDateEl).add(4, 'days').format('ll');
+var dayFiveEl = moment(todayDateEl).add(5, 'days').format('ll');
+
