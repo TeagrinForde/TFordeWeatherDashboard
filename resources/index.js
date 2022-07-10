6ecab14f9f9f266a304f88ccc256d04ae3a50fcc
Teagrin Forde <teagrinb@gmail.com>
2022-07-09 20:52:55 -0400

    finished api fetch functions in js

--- resources/index.js
+++ resources/index.js
@@ -1,57 +1,72 @@
 var apiKey = '53365e58d7d5982b4bd03b8ffaccde6d';
 var cityArrayEl = []; //where the data is collected
 
-function saveCity () {
-    var cityInputEl = document.getElementById('cityInput');
-    cityArrayEl.push(cityInputEl.value);
-    localStorage.setItem('cityList', JSON.stringify(cityArrayEl)); //where the city input is stored
-    displayCity();
-    displayResultEl (cityInputEl.value); //display list
+function saveCity() {
+  var cityInputEl = document.getElementById('cityInput');
+  cityArrayEl.push(cityInputEl.value);
+  localStorage.setItem('cityList', JSON.stringify(cityArrayEl)); //where the city input is stored
+  displayCity();
+  displayResultEl(cityInputEl.value); //display list
 }
 
-function displayCity () {
-    var cityListEl = document.getElementById('cityList');
-    if (localStorage.getItem('cityList')) {
-        cityArrayEl = JSON.parse(localStorage.getItem('cityList'))
-        for (var i = 0; i < cityArrayEl.length; i++) {
-            var cityEl = document.createElement('li');
-            cityEl.textContent = cityArrayEl[i];
-            cityListEl.appendChild(cityEl);
-        }
+function displayCity() {
+  var cityListEl = document.getElementById('cityList');
+  if (localStorage.getItem('cityList')) {
+    cityArrayEl = JSON.parse(localStorage.getItem('cityList'))
+    for (var i = 0; i < cityArrayEl.length; i++) {
+      var cityEl = document.createElement('li');
+      cityEl.textContent = cityArrayEl[i];
+      cityListEl.appendChild(cityEl);
     }
+  }
 }
 displayCity();
 
-function displayResultEl (cityInput) {
-    //pull time from moment website and user city input to display data
-    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`;
+function displayResultEl(cityInput) {
+  //pull time from moment website and user city input to display data
+  var currentUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}&units=imperial&cnt=6`;
 
-    fetch(currentUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        document.getElementById('dateInfo').innerHTML = moment(data.dt, 'X').format('MM/DD/YYYY');
-        document.getElementById('cityName').innerHTML = data.name;
-        document.getElementById('cityTemp').innerHTML = data.main.temp;
-        document.getElementById('cityWind').innerHTML = data.wind.speed;
 
-        document.getElementById('cityHumid').innerHTML = data.main.humidty; //not working
-        document.getElementById('cityIcon').innerHTML = data.weather[0].icon;//not working -- stopped here
-        
-        var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-      })
-      for (var i=0; i< data.length; i++) {
-        var temperature = document.createElement('li');
-        var wind = document.createElement('li');
-        var humidty = document.createElement('li');
-        var icon = document.createElement('li');
+  fetch(currentUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      document.getElementById('cityName').innerHTML = data.city.name;
 
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
+      document.getElementById('dateInfo').innerHTML = moment(data.list[0].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('cityTemp').innerHTML = data.list[0].main.temp;
+      document.getElementById('cityHumid').innerHTML = data.list[0].main.humidity;
+      document.getElementById('cityWind').innerHTML = data.list[0].wind.speed;
+      document.getElementById('cityIcon').innerHTML = data.list[0].weather[0].icon; //make function to pull icon id to creat pic
+      //uv index
 
-        ol.appendChild(li);
-    }
+      document.getElementById('dateOne').innerHTML = moment(data.list[1].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('tempOne').innerHTML = data.list[1].main.temp;
+      document.getElementById('humidOne').innerHTML = data.list[1].main.humidity;
+      document.getElementById('windOne').innerHTML = data.list[1].wind.speed;
+
+      document.getElementById('dateTwo').innerHTML = moment(data.list[2].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('tempTwo').innerHTML = data.list[2].main.temp;
+      document.getElementById('humidTwo').innerHTML = data.list[2].main.humidity;
+      document.getElementById('windTwo').innerHTML = data.list[2].wind.speed;
+
+      document.getElementById('dateThree').innerHTML = moment(data.list[3].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('tempThree').innerHTML = data.list[3].main.temp;
+      document.getElementById('humidThree').innerHTML = data.list[3].main.humidity;
+      document.getElementById('windThree').innerHTML = data.list[3].wind.speed;
+
+      document.getElementById('dateFour').innerHTML = moment(data.list[4].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('tempFour').innerHTML = data.list[4].main.temp;
+      document.getElementById('humidFour').innerHTML = data.list[4].main.humidity;
+      document.getElementById('windFour').innerHTML = data.list[4].wind.speed;
+
+      document.getElementById('dateFive').innerHTML = moment(data.list[5].dt, 'X').format('MM/DD/YYYY');
+      document.getElementById('tempFive').innerHTML = data.list[5].main.temp;
+      document.getElementById('humidFive').innerHTML = data.list[5].main.humidity;
+      document.getElementById('windFive').innerHTML = data.list[5].wind.speed;
+    });
 }
+
 document.getElementById('basic-addon2').addEventListener('click', saveCity)
